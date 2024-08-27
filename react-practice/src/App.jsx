256259d82eccfdb70b43432720fc56037564159a
Keisei Akiya <keisei.akiya@gmail.com>
2024-08-27 14:39:49 +0900

    i'm done section 6, react practice

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,15 +1,36 @@
-import {ColorfulMessage} from "./components/ColorfulMessage.jsx";
+import { useState, useEffect } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
-const App = () => {
-  const onClickButton = () => alert();
-  return (
+export const App = () => {
+  console.log("--App--");
+  const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(false);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1);
+  };
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]);
+
+    return (
     <>
-    <h1 style={{color: "red"}}>Hello</h1>
-    <ColorfulMessage color="blue">what's up?</ColorfulMessage>
-    <ColorfulMessage color="green">good.</ColorfulMessage>
-    <button onClick={onClickButton}>button</button>
+      <h1 style={{color: "red"}}>Hello World</h1>
+      <ColorfulMessage color="blue">what's up?</ColorfulMessage>
+      <ColorfulMessage color="green">good.</ColorfulMessage>
+      <button onClick={onClickCountUp}>count up</button>
+      <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>(^o^)</p>}
     </>
   )
 }
-
-export default App;
